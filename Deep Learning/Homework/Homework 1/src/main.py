5ac503a5485a741498dffe208bd31d8614d62d1b
Jacob Cassady <jtcass01@louisville.edu>
2020-01-27 18:16:23 -0500

    attempting to generate train data using multivariate normal.

--- Deep Learning/Homework/Homework 1/src/main.py
+++ Deep Learning/Homework/Homework 1/src/main.py
@@ -172,7 +172,7 @@ def question_4():
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     model.fit(features, targets, epochs=100, batch_size=32, verbose=1)
 
-    plotModelOut(features, targets, model)
+    plotModelOut(features[0, :], features[1, :], model)
 
     """
 
