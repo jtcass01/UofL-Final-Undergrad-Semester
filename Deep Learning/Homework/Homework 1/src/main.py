5a18bee90f8d56a479844752a418223461588c46
Jacob Cassady <jtcass01@louisville.edu>
2020-01-27 18:20:05 -0500

    attempting to generate train data using multivariate normal.

--- Deep Learning/Homework/Homework 1/src/main.py
+++ Deep Learning/Homework/Homework 1/src/main.py
@@ -124,7 +124,7 @@ def plot_decision_boundary(X, y, model, steps=1000, cmap='RdBu'):
     train_labels = model.predict(X)
     ax.scatter(X[:,0], X[:,1], c=y, cmap=cmap, lw=0)
 
-    ax.show()
+    plt.show()
 
     return fig, ax
 
